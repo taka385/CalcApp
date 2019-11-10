017fd6c58ba78ee8d37e2c806d53a9b05639d9c8
taka385 <sonny.taka51@gmail.com>
2019-11-10 16:12:25 +0900

    四則1110

--- app/src/main/java/com/example/calcapp/MainActivity.kt
+++ app/src/main/java/com/example/calcapp/MainActivity.kt
@@ -11,7 +11,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(),View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        
         setContentView(R.layout.activity_main)
 
         addition.setOnClickListener(this)
